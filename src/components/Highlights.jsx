f93625e6a52d7f829cece379f429a6be48f73150
zahraa Meky <eng.alzahraa.meky@gmail.com>
2024-04-13 16:55:14 +0400

    create Highlights component
    which contain Video Carousel with nice animation created b gsap

--- src/components/Highlights.jsx
+++ src/components/Highlights.jsx
@@ -1,8 +1,32 @@
+import gsap from "gsap";
+import { useGSAP } from "@gsap/react";
+import {watchImg,rightImg} from '../utils'
+import VideoCarousel from "./VideoCarousel ";
 const Highlights = () => {
+  useGSAP(() => {
+    // gsap code here...
+    gsap.to("#title", {opacity: 1,y:0});
+    gsap.to(".link", {opacity: 1,y:0,duration:1,stagger:0.25});
+  }, []); 
   return (
-    <div>
-      
-    </div>
+    <section id="highlights" className="w-screen overflow-hidden h-full bg-zinc common-padding">
+      <div className="screen-max-width">
+        <div className="mb-12 items-end justify-between w-full md:flex">
+          <h1 id="title" className="section-heading">Get the highlights.</h1>
+          <div className="flex flex-wrap items-end gap-5">
+            <p className="link">
+              Watch the film
+              <img src={watchImg} alt="watchImg"/>
+            </p>
+            <p className="link">
+              Watch the event
+              <img src={rightImg} alt="watchImg" className="ml-2"/>
+            </p>
+          </div>
+        </div>
+        <VideoCarousel/>
+      </div>
+    </section>
   )
 }
 
