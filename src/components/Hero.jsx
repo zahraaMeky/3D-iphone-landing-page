694b5b828dc3ed24531fc72be7dd451efda1b417
zahraa Meky <eng.alzahraa.meky@gmail.com>
2024-04-13 06:50:08 +0400

    Hero Section
    In Hero display 2 different video versions, one small and one big depending on the screen size. To achieve this, I utilized useEffect to add a listener to the screen size to set the video. I also employed GSAP to animate text.

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,8 +1,44 @@
+import gsap from "gsap";
+import { useGSAP } from "@gsap/react";
+import { useState, useEffect } from "react";
+import {smallHeroVideo ,heroVideo } from "../utils"
 const Hero = () => {
+  const [videoSrc, setVideoSrc] = useState(window.innerWidth <760 ? smallHeroVideo:heroVideo); 
+useGSAP(() => {
+  // gsap code here...
+  gsap.to("#hero", {opacity: 1,delay:2});
+  gsap.to("#cta", {opacity: 1,delay:2,y:-50});
+}, []); 
+
+  const handleVideoSrcSet =()=>{
+    if(window.innerWidth<760){
+        setVideoSrc(smallHeroVideo)
+    }else{
+      setVideoSrc(heroVideo)
+    }
+  }
+
+  useEffect(() => {
+    window.addEventListener('resize',handleVideoSrcSet)
+    return ()=>{
+      window.removeEventListener("resize",handleVideoSrcSet)
+    }
+  },[]);
   return (
-    <div>
-      
-    </div>
+    <section className="w-full bg-black relative nav-height">
+      <div className="h-5/6 w-full flex-center flex-col">
+        <p id="hero" className="hero-title">iPhone 15 Pro </p>
+        <div className="md:w-10/12 w-9/12">
+          <video className="pointer-events-none"autoPlay muted playsInline={true} key={videoSrc}>
+            <source src={videoSrc} type="video/mp4"></source>
+          </video>
+        </div>
+      </div>
+      <div id="cta" className="flex flex-col items-center opacity-0 translate-y-0">
+          <a id="highlights" className="btn">Buy</a>
+          <p className="text-xl font-normal">From $199/month or $999</p>
+      </div>
+    </section>
   )
 }
 
